f6b07694fa91bf87ed4ba3da2be6cef8f29ce675
Christian <102103684+CyberBot31@users.noreply.github.com>
2023-06-12 19:17:29 -0300

    Add files via upload

---
+++ QuizP5/quiz-app/src/data/questions.js
@@ -0,0 +1,46 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "O que é Front-end?",
+      answer: "Parte de um sistema é visivel e interativa ao usuário.",
+      options: [
+        "Parte de um sistema que é oculta para o usuário.",
+        "Parte de um sistema é visivel e interativa ao usuário.",
+        "Parte lógica que recebe as regras de negócio.",
+        "Nenhuma das alternativas anteriores.",
+      ],
+    },
+    {
+      numb: 2,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "O que é o React JS?",
+      answer: "Uma poderosa biblioteca JavaScript.",
+      options: [
+        "Uma poderosa biblioteca JavaScript.",
+        "Uma linguagem de Programação.",
+        "Um servidor de Cloud.",
+        "Todas as respostas anteriores.",
+      ],
+    },
+    {
+      numb: 3,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Quais são as principais tecnologias do mundo Front-end?",
+      answer: "HTML, CSS e JavaScript.",
+      options: [
+        "Java, golang e Python.",
+        "AWS, Google Cloud e Azure.",
+        "Kotlin, HTML e CSS.",
+        "HTML, CSS e JavaScript.",
+      ],
+    },
+  ];
+  
